6d629ea24f53b52bc205d07288ddac590539d52f
harshitemp <harshitemp28@gmail.com>
2024-09-09 14:00:35 +0530

    changes19

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { LoginComponent } from './login/login.component';
+import { SignUpReactiveComponent } from './sign-up-reactive/sign-up-reactive.component';
+import { LoginReactiveComponent } from './login-reactive/login-reactive.component';
+
+export const routes: Routes = [
+  { path: 'signup', component: SignUpComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'signup-reactive', component: SignUpReactiveComponent },
+  { path: 'login-reactive', component: LoginReactiveComponent },
+  { path: '', redirectTo: '/signup', pathMatch: 'full' }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
